38975f156fc0e157ff6bc6e070cc9da2e567db72
Mona Mohamed <42801687+monaelhoby@users.noreply.github.com>
2018-10-26 00:31:36 +0200

    Update show.js

--- src/components/show.js
+++ src/components/show.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class Show extends Component {
-
-  render(){
-    const string = this.props.data.join(' ')
-    return (
-      <div style={this.props.style}>{string}</div>
-    )
-  }
-
-}
-
+const Show = (props=>
+     <div style={props.style}>{props.data}</div>
+)
 export default Show;
